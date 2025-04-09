2f4a1afc923ac570ff821b22924f3e711b7bab84
Kasador <hunterstevenshaw@gmail.com>
2025-04-09 05:25:04 -0700

    Added Netlify deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,3 +3,15 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({});
+
+// https://docs.astro.build/en/guides/deploy/netlify/
+
+// import { defineConfig } from 'astro/config';
+// import netlify from '@astrojs/netlify/functions';
+
+// export default defineConfig({
+//   output: 'server',
+//   adapter: netlify({
+//     edgeMiddleware: true
+//   }),
+// });
